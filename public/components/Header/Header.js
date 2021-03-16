a5ce55ec9ef3d585ace5406eb1dc74cbcbe0716f
Ilia Muravev <degtyarev327@gmail.com>
2021-03-16 15:23:21 +0300

    add redirect to main page on logo click and remove header rerender

--- public/components/Header/Header.js
+++ public/components/Header/Header.js
@@ -3,11 +3,13 @@ import {HttpRequest} from "../../modules/http.js";
 const http = new HttpRequest('/api');
 const app = document.getElementById('app');
 const submitButton = document.getElementsByClassName('header__search-button').item(0);
+const logo = document.getElementsByClassName('header__logo-content').item(0);
 
 const mainPageTemplate = Handlebars.templates['views/MainPage/MainPage'];
 const catalogPageTemplate = Handlebars.templates['views/CatalogPage/CatalogPage'];
 
 submitButton.addEventListener('click', sendSearchRequest);
+logo.addEventListener('click', goMainPage);
 
 async function sendSearchRequest() {
     const searchString = document.getElementsByClassName('header__search-input').item(0).value;
@@ -20,9 +22,20 @@ async function sendSearchRequest() {
                     let catalogCtx = res.body;
                     catalogCtx.request = searchString;
 
-                    app.innerHTML = headerTemplate();
-                    app.innerHTML += catalogPageTemplate(res.body);
+                    const header = document.getElementsByClassName('header').item(0);
+                    header.nextElementSibling.remove();
+
+                    const content = catalogPageTemplate(res.body);
+                    header.insertAdjacentHTML('afterend', content);
                 }
             })
     }
+}
+
+function goMainPage() {
+    const header = document.getElementsByClassName('header').item(0);
+    header.nextElementSibling.remove();
+
+    const content = mainPageTemplate();
+    header.insertAdjacentHTML('afterend', content);
 }
